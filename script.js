f5fb553107f8bc9100551ca71bfa8bee4ea17319
shefan khan <118080960+shefankhan@users.noreply.github.com>
2023-05-12 17:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+class OutOfRangeError extends Error {
+	constructor(message) {
+    super(message);
+    this.name = 'OutOfRangeError';
+  }
+}
+
+class InvalidExprError extends Error {
+	constructor(message) {
+    super(message);
+    this.name = 'InvalidExprError';
+  }
+}
